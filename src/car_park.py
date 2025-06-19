cb53dd4ae1d43e92581a66f1a4a1767d6bdbb65d
J169457 <J169457@tafe.wa.edu.au>
2025-06-20 02:14:50 +0800

    Updated CarPark class

--- src/car_park.py
+++ src/car_park.py
@@ -1,9 +1,24 @@
+from sensor import Sensor
+from display import Display
+
+
 class CarPark:
-    def __init__(self, location="Unknown", capacity=0, plates=None, displays=None):
+    def __init__(self, location="Unknown", capacity=0, plates=None, displays=None, sensors=None):
         self.location = location
         self.capacity = capacity
         self.plates = plates or []
         self.displays = displays or []
+        self.sensors = sensors or []
 
     def __str__(self):
         return f"Car park location is {self.location}, with {self.capacity} bays free"
+
+    def register(self, component):
+        if not isinstance(component, (Sensor, Display)):
+            raise TypeError("Object must be a Sensor or Display")
+
+        if isinstance(component, Sensor):
+            self.sensors.append(component)
+        elif isinstance(component, Display):
+            self.displays.append(component)
+
