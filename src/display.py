5dd617a8b85200931566b48fd8e6603c09196b47
J169457 <J169457@tafe.wa.edu.au>
2025-06-20 01:59:36 +0800

    Wrote Sensor and Display classes

--- src/display.py
+++ src/display.py
@@ -1,3 +1,9 @@
 class Display:
-    pass
+    def __init__(self, id, message="", is_on=False):
+        self.id = id
+        self.message = message
+        self.is_on = is_on
+
+    def __str__(self):
+        return f"Display {self.id}: {self.message}"
 
--- src/sensor.py
+++ src/sensor.py
@@ -1,3 +1,18 @@
 class Sensor:
+    def __init__(self, id, is_active, car_park):
+        self.id = id
+        self.is_active = is_active
+        self.car_park = car_park
+
+    def __str__(self):
+        status = "active" if self.is_active else "inactive"
+        return f"Sensor {self.id} is {status}"
+
+
+class EntrySensor(Sensor):
+    pass
+
+
+class ExitSensor(Sensor):
     pass
 
