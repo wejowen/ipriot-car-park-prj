174e27a2c14e71351e598a978f417d97f1bee5c1
J169457 <J169457@tafe.wa.edu.au>
2025-06-20 01:52:16 +0800

    Wrote CarPark class

--- src/car_park.py
+++ src/car_park.py
@@ -1,3 +1,9 @@
 class CarPark:
-    pass
+    def __init__(self, location="Unknown", capacity=0, plates=None, displays=None):
+        self.location = location
+        self.capacity = capacity
+        self.plates = plates or []
+        self.displays = displays or []
 
+    def __str__(self):
+        return f"Car park location is {self.location}, with {self.capacity} bays free."
