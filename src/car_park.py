e343f0b2ffedf8562b82ac99c5efd92229ec7137
J169457 <J169457@tafe.wa.edu.au>
2025-06-20 22:32:34 +0800

    Added log files for testing

--- src/car_park.py
+++ src/car_park.py
@@ -1,15 +1,18 @@
 from sensor import Sensor
 from display import Display
 from datetime import datetime
+from pathlib import Path
 
 
 class CarPark:
-    def __init__(self, location="Unknown", capacity=0, plates=None, displays=None, sensors=None):
+    def __init__(self, location="Unknown", capacity=0, plates=None, displays=None, sensors=None, log_file=Path("log.txt")):
         self.location = location
         self.capacity = capacity
         self.plates = plates or []
         self.displays = displays or []
         self.sensors = sensors or []
+        self.log_file = log_file if isinstance(log_file, Path) else Path(log_file)
+        self.log_file.touch(exist_ok=True)
 
     def __str__(self):
         return f"Car park location is {self.location}, with {self.capacity} bays free"
@@ -26,11 +29,13 @@ class CarPark:
     def add_car(self, plate):
         self.plates.append(plate)
         self.update_displays()
+        self._log_car_activity(plate, "entered")
 
     def remove_car(self, plate):
         if plate in self.plates:
             self.plates.remove(plate)
             self.update_displays()
+            self._log_car_activity(plate, "exited")
         else:
             raise ValueError(f"Plate '{plate}' not found in car park.")
 
@@ -48,3 +53,6 @@ class CarPark:
         for display in self.displays:
             display.update(data)
 
+    def _log_car_activity(self, plate, action):
+        with self.log_file.open("a") as f:
+            f.write(f"{plate} {action} at {datetime.now():%Y-%m-%d %H:%M:%S}\n")
--- tests/test_car_park.py
+++ tests/test_car_park.py
@@ -1,8 +1,11 @@
 import unittest
+from pathlib import Path
 from car_park import CarPark
 
 
 class TestCarPark(unittest.TestCase):
+    log_filename = "new_log.txt"
+
     def setUp(self):
         self.car_park = CarPark("123 Example Street", 100)
 
@@ -13,6 +16,7 @@ class TestCarPark(unittest.TestCase):
         self.assertEqual(self.car_park.plates, [])
         self.assertEqual(self.car_park.displays, [])
         self.assertEqual(self.car_park.available_bays, 100)
+        self.assertEqual(self.car_park.log_file, Path("log.txt"))
 
     def test_add_car(self):
         self.car_park.add_car("FAKE-001")
@@ -43,6 +47,31 @@ class TestCarPark(unittest.TestCase):
         with self.assertRaises(TypeError):
             self.car_park.register("Not a Sensor or Display")
 
+    def test_log_file_created(self):
+        new_carpark = CarPark("123 Example Street", 100, log_file="new_log.txt")
+        self.assertTrue(Path("new_log.txt").exists())
+
+    def tearDown(self):
+        Path(self.log_filename).unlink(missing_ok=True)
+
+    def test_car_logged_when_entering(self):
+        self.car_park.add_car("NEW-001")
+        with self.car_park.log_file.open() as f:
+            last_line = f.readlines()[-1]
+        self.assertIn("NEW-001", last_line)
+        self.assertIn("entered", last_line)
+        self.assertIn("\n", last_line)
+
+    def test_car_logged_when_exiting(self):
+        self.car_park.add_car("NEW-001")
+        self.car_park.remove_car("NEW-001")
+        with self.car_park.log_file.open() as f:
+            last_line = f.readlines()[-1]
+        self.assertIn("NEW-001", last_line)
+        self.assertIn("exited", last_line)
+        self.assertIn("\n", last_line)
+
 
 if __name__ == "__main__":
     unittest.main()
+
